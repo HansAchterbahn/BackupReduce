dab2bd80f0cd8c32170803f6d0480b014a6f118e
Mario Hesse <m.tec@gfisch.de>
2024-04-01 21:02:24 +0200

    [Optimized] The script is now able to process multiple folders with backup files with multiple backup prefixes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import re
 from datetime import datetime
 import natsort
 import logging
+from collections import defaultdict
 
 def find_first_unique_item_in_list(items:list, amount:int=0, direction:str= "backward"):
     # looks for the first unique entries in a given list and returns these entries and their IDs
@@ -36,15 +37,49 @@ def reduce_backup_files(file_amount:int = 7, file_path:str = "", remove_folder_p
     # - remove_folder_path  :str:   path/folder to move the unwanted files to (ex. remove, /dev/null)
 
     # Set up logging
-    #logging.basicConfig(filename='remove.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s')
-    logging.basicConfig(filename=file_path + '.log', format='%(message)s', level=logging.INFO)
-    logging.info("["+str(datetime.now())+"]")
+    #logging.basicConfig(filename="reduce" + '.log', format='%(message)s', level=logging.INFO)
+    logging.basicConfig(format='%(message)s', level=logging.INFO)
+    logging.info("["+file_path+" | "+str(datetime.now())+"]")
 
 
     # read the file list and sort it
     files = [f for f in os.listdir(file_path) if os.path.isfile(os.path.join(file_path, f))]
     files = natsort.natsorted(files)
+    files_ignored = []
 
+    prefixes_dict = dict()
+    prefixes = set([])
+    for file in files:
+        match = re.search("(^[^_]*)_?([0-9]{4}-[0-9]{2}-[0-9]{2})", file)
+        if match:
+            prefix = match.group(1)
+            if prefix in prefixes:
+                prefixes_dict[prefix].append(file)
+            else:
+                prefixes_dict[prefix] = [file]
+                prefixes.add(prefix)
+        else:
+            files_ignored.append(file)
+
+
+    # create remove folder if not exist
+    try:
+        os.makedirs(remove_folder_path)
+    except:
+        pass
+
+    # generate keep and remove files list & remove files from remove file list
+    for prefix in prefixes_dict:
+        keep_files, remove_files = calculate_keep_and_remove_files(prefixes_dict[prefix])
+        logging.info("Remove prefix list %s: \n- %s", prefix, remove_files)
+        for file in remove_files:
+            os.rename(str(file_path + "/" + file), str(remove_folder_path + "/" + file))
+
+
+
+
+
+def calculate_keep_and_remove_files(files:list, backup_amount:int=7, yearly:bool=True, monthly:bool=True, weekley:bool=True, daily:bool=True):
     # create empty lists for yearly, monthly, weekly and daily backups
     yearly = []
     monthly = []
@@ -53,23 +88,25 @@ def reduce_backup_files(file_amount:int = 7, file_path:str = "", remove_folder_p
 
     # sort files in to the yearly, monthly, weekly and daily backup lists
     for i, file in enumerate(files):
-        date = re.match("[0-9]{4}-[0-9]{2}-[0-9]{2}", file).group()
-        yearly.append(datetime.strptime(date, "%Y-%m-%d").strftime("%Y"))
-        monthly.append(datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%m"))
-        weekly.append(datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%V"))
-        daily.append(datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%m-%d"))
+        match = re.search("[0-9]{4}-[0-9]{2}-[0-9]{2}", file)
+        if match:
+            date = match.group()
+            yearly.append(datetime.strptime(date, "%Y-%m-%d").strftime("%Y"))
+            monthly.append(datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%m"))
+            weekly.append(datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%V"))
+            daily.append(datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%m-%d"))
 
     # find first unique items in wanted amount and direction for yearly, monthly, weekly and daily backups
-    yearly = find_first_unique_item_in_list(yearly, file_amount, "backward")
-    monthly = find_first_unique_item_in_list(monthly, file_amount, "backward")
-    weekly = find_first_unique_item_in_list(weekly, file_amount, "backward")
-    daily = find_first_unique_item_in_list(daily, file_amount, "backward")
+    yearly = find_first_unique_item_in_list(yearly, backup_amount, "backward")
+    monthly = find_first_unique_item_in_list(monthly, backup_amount, "backward")
+    weekly = find_first_unique_item_in_list(weekly, backup_amount, "backward")
+    daily = find_first_unique_item_in_list(daily, backup_amount, "backward")
 
     # log separat keep lists
-    logging.info("Years match: %s", yearly)
-    logging.info("Month match: %s", monthly)
-    logging.info("Weeks match: %s", weekly)
-    logging.info("Days match: %s",  daily)
+    #logging.info("Years match: %s", yearly)
+    #logging.info("Month match: %s", monthly)
+    #logging.info("Weeks match: %s", weekly)
+    #logging.info("Days match: %s",  daily)
 
     # merge all wanted unique items together in the keep file list by looping to every separate list
     keep_files = set([])
@@ -79,7 +116,7 @@ def reduce_backup_files(file_amount:int = 7, file_path:str = "", remove_folder_p
             keep_ids.add(item[0])
             keep_files.add(files[item[0]])
 
-    logging.info("Keep files: %s", natsort.natsorted(keep_files))
+    #logging.info("Keep files: %s", natsort.natsorted(keep_files))
 
     # create list of files to be removed
     remove_files = []
@@ -87,18 +124,14 @@ def reduce_backup_files(file_amount:int = 7, file_path:str = "", remove_folder_p
         if file not in keep_files:
             remove_files.append(file)
 
-    # create remove folder if not exist
-    try:
-        os.makedirs(remove_folder_path)
-    except:
-        pass
-
-    logging.info("Remove list: %s", remove_files)
-
-    # remove files from remove list
-    for file in remove_files:
-        os.rename(str(file_path + "/" + file), str(remove_folder_path + "/" + file))
+    return keep_files, remove_files
 
 
 if __name__ == "__main__":
-    reduce_backup_files(file_amount=7, file_path=".Contacts-Backup", remove_folder_path=".Contacts-Backup/remove")
+    # user Settings
+    backup_amount = 7
+    file_paths = [".Contacts-Backup", ".Calendar-Backup", ".KeePassXC-Backup"]
+
+    # run script
+    for file_path in file_paths:
+        reduce_backup_files(file_amount=7, file_path=file_path, remove_folder_path=file_path+"/remove")
